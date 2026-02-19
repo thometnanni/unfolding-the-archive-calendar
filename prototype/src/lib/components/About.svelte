b39b1936f9fe5a6f1926d29da159ca5c1f5ab912
Giacomo Nanni <gn.nanni@gmail.com>
2026-02-19 09:53:38 +0100

    chore: updated text

--- prototype/src/lib/components/About.svelte
+++ prototype/src/lib/components/About.svelte
@@ -15,9 +15,10 @@
 		<p class="my-2">
 			This visualisation was created by
 			<a href="https://krisenstab.net/">Krisenstab</a> and displays 14 projects by
-			<a href="https://www.mvrdv.com/">MVRDV</a>. It shows the time of day drawings, images,
-			documents and other files have been created and emphasises periods of inactivity when set to
-			group files by year, month, or day.
+			<a href="https://www.mvrdv.com/">MVRDV</a>. It shows when drawings, images, documents and
+			other files have been created. Through yearly, monthly, or daily overviews it traces the
+			periods of high activity in the file creation as well as moments when things fell still. It
+			allows to browse the archive by following the dynamics of the digital design process.
 		</p>
 		<button
 			class="cursor-pointer sticky top-4 bg-slate-700/10 rounded-full px-2 py-1 my-1 mb-4 text-slate-700 text-xs -ml-1 backdrop-blur-xs"
@@ -28,50 +29,40 @@
 
 		{#if showMore}
 			<p>
-				Born-digital architecture archives present unique opportunities to study and interpret the
-				intertwined relationship between built environment, digital cultures, and social change.
-				However, due to technical limitations of the proprietary software, most of their contents
-				remain inaccessible to the public at large, posing limits to both research and discovery. In
-				the Dutch National Collection for Architecture and Urban Planning, where this research was
-				conducted, currently as much as 60% of the collection is inaccessible.
-			</p>
-			<p>
-				Unfolding the Archive uses select properties of Computer Aided Design (CAD) Drawing (DWG)
-				and Drawing Interchange Format (DXF) as a dominant CAD file formats found in architecture
-				practice from their introduction in the early 1980s throughout the 2000s. It proposes new
-				conceptual approaches to access archives including CAD archival records based on their
-				embedded metadata. By doing so it allows the users to form a better understanding of born
-				digital archives as data, not a collection of individual files. It allows to understand the
-				relationships between archival objects and better contextualise archives, providing insights
-				into the structure and unique characteristics of individual collections, that are impossible
-				to see with a naked eye and without specialist technical knowledge. On the most direct level
-				it aims to stimulate users to engage with born digital collections on the level of relations
-				and help them to identify research questions prior to requesting files. It also hopes for
-				creating new insights that would otherwise only be available to researchers with highly
-				technical knowledge or significant funding and through that contribute to democratisation of
-				research with born digital architecture collections, making them more multivocal.
-			</p>
-			<p>
-				Drawing from the concept of generous interfaces, <a
+				Drawing from the concept of generous interfaces by Mitchel Whitelaw, <a
 					href="https://unfolding-the-archive-prototypes.krisenstab.net">three prototypes</a
-				> have been initially developed within this project. After careful evaluation one of the prototypes
-				was selected, further refined and is now presented here.
+				> have been developed within Unfolding the Archive. Based on the results of user-testing, the
+				calendar view was selected, further refined and is now presented here.
+			</p>
+			<p>
+				The calendar view is based on a temporal distribution of individual records. It displays
+				what types of records were cerated (drawing, image, document, other) when. It provides
+				insights in production spikes, shows information on work culture (i.e. 9 to 5 work model,
+				late night shifts), but also project dynamics by indicating moments of inactivity (i.e.
+				waiting for financing, consultations, permissions, other work etc.). It allows to pinpoint
+				potential derivatives within the archive (i.e. based on time proximity between two files
+				with a similar name and a different file format), and sheds light on archiving (i.e. moving
+				large amounts of files to a new location).
 			</p>
 			<p>
 				Unfolding the Archive looked at how select properties of popular Computer Aided Design (CAD)
-				files – Drawing (DWG) and Drawing Interchange Format (DXF) can be used for new forms of
-				access to architecture archives based on their embedded metadata. The resulting prototype
-				understands born digital archives as data, not a collection of individual objects. They
-				focus on visualizing the relationships between archival objects, finding ways to
-				contextualise the archives by providing insights into the structure and unique
-				characteristics of individual collections, that are impossible to see with a naked eye and
-				without specialist technical knowledge. On the most direct level the research aimed to
-				stimulate users to engage with born digital collections on the level of relations and help
-				them to identify research questions prior to requesting files. It also experimented with
-				different ways to create new data-driven insights that would otherwise only be available to
-				researchers with highly technical knowledge or significant funding. Through that it would
-				like to contribute to the democratisation of research with born digital architecture
-				collections, inviting more multivocal approaches to research.
+				files – Drawing (DWG) and Drawing Interchange Format (DXF) can be accessed based on their
+				embedded metadata. The resulting prototypes understand born digital archives as data, not a
+				collection of individual objects. They focus on visualizing the relationships between
+				archival objects and provide insights into the characteristics of individual collections,
+				which are often invisible.
+			</p>
+			<p>
+				Born-digital architecture archives present unique opportunities to study and interpret the
+				intertwined relationship between built environment, digital cultures, and social change.
+				However, due to technical limitations most of their contents remain inaccessible to the
+				public at large, posing limits to both research and discovery. In the Dutch National
+				Collection for Architecture and Urban Planning, where this research was conducted, currently
+				as much as 60% of the born digital collection is inaccessible. By developing conceptual
+				approaches and prototypes, this research demonstrates how relational and experimental
+				interfaces can visualize connections between archival objects, stimulate early-stage
+				research, and democratize access to born-digital architecture collections. In this way it
+				aims to invite new and more unexpected approaches to research.
 			</p>
 			<p>
 				The project has been supported by NWO Museum Grant in 2023. It was conducted by the Dutch
